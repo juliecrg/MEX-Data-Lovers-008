d2fefab686b9d18cb6f16c0971f2c532f554adf4
MontseRV <montserrat.rova@gmail.com>
2019-07-02 15:17:46 -0500

    está modificado el javascript con la función que permite jalar los datos del json y los pinta en el html

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const data = POKEMON.pokemon; //Traer la data y guardarla en una variable
+const botonNombre = document.getElementById("ButtonA")
+const tarjetas =document.getElementById("tarjetas-pokemon")
+
+const imprimData = () => { //variable para manipular la data
+    let str = "";
+    data.forEach(element =>{ //foeEach sirve para recorrer los arreglos dentro de un objeto
+        str += `<div class = "PokemonBox"> <p>${element.name}</p> 
+        <img src="${element.img}"></img>
+        <p>Tipo: ${element.type}</p>
+        <p>Caramelos: ${element.candy_count} </p>
+        <p>Hora: ${element.spawn_time} </p>
+        </div>`
+    });
+
+    tarjetas.innerHTML =str
+}
+
+botonMos.addEventListener("click", imprimData)
